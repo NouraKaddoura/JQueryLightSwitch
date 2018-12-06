27de06b64fc5721c9782bb26e07d393ec6d0e938
Noura Kaddoura <nourakaddoura@gmail.com>
2018-12-05 17:00:43 -0800

    compareAdded

---
+++ Compare Two Variables/js/index.js
@@ -0,0 +1,21 @@
+
+//Making Decisions
+
+//It's either true or False (like booleans)
+//if you are greater than 18 you are an adult
+//if (age > 18){
+//    document.write("You are an adult.")
+//}
+
+
+
+
+    //Hide the comparison symbol
+    //Get the variables
+    //Set the values we wanna check
+    //Equals AND same type
+    //Equals BUT type is converted
+    //Greater than
+    //Less than
+    //Hmmmmmm
+    //Fade in the comparison symbol

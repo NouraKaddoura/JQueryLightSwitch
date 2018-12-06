4e3f1e5640e5c913c72ebfef784cca26eab5d9d6
Noura Kaddoura <nourakaddoura@gmail.com>
2018-12-05 16:56:53 -0800

    addedGame

---
+++ video_Game/js/index.js
@@ -0,0 +1,48 @@
+//Setup variables
+var total = 15;
+var level = 1;
+var gameOver = false;
+
+//Define functions
+
+    //what should we do?
+
+
+
+
+    //Set your health to the variable in line2
+    
+
+    //Set his health to the variable in line2
+
+    //Set the actionImage to the main banner
+
+
+//Start game by "invoking" the start function with ()
+
+
+//Add Click Events
+
+    //invoke start
+
+
+
+    //Get the value of his health
+    
+
+    //Set the value of the punch strength
+   
+
+    //If his health after a punch is less than or equal to 0 then Game Over
+  
+    
+      
+
+    //Else...go ahead and take away some of his health
+
+
+    //Set the actionImage to you punching him
+
+
+
+
